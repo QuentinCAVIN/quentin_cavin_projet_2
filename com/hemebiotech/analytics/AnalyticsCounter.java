4627c3dcd29d1852d8db424e02d58de8ae02d5b1
Quentin Cavin <h67c6qq58tc5-student@anonymous-codio.com>
2023-03-16 11:42:07 +0000

    writer implementation

--- com/hemebiotech/analytics/AnalyticsCounter.java
+++ com/hemebiotech/analytics/AnalyticsCounter.java
@@ -5,25 +5,22 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	
+	private static int headCount = 0;	
 	private static int rashCount = 0;		
 	private static int pupilCount = 0;	
-	
+
 	public static void main(String args[]) throws Exception {
 		// first get input
 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
 		String line = reader.readLine();
-
-		int i = 0;	
-		int headqchesCount = 0;	
+		
 		while (line != null) {
-			i++;	
 			System.out.println("symptom from file: " + line);
 			if (line.equals("headache")) {
 				headCount++;
 				System.out.println("number of headaches: " + headCount);
 			}
-			else if (line.equals("rush")) {
+			else if (line.equals("rash")) {
 				rashCount++;
 			}
 			else if (line.contains("pupils")) {
@@ -35,7 +32,7 @@ public class AnalyticsCounter {
 		
 		// next generate output
 		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
+		writer.write("headache: " + headCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
 		writer.write("dialated pupils: " + pupilCount + "\n");
 		writer.close();
--- com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -1,3 +1,4 @@
+
 package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
@@ -18,22 +19,25 @@ public class ReadSymptomDataFromFile implements ISymptomReader {
 	 * 
 	 * @param filepath a full or partial path to file with symptom strings in it, one per line
 	 */
-	public ReadSymptomDataFromFile (String filepath) {
+	public ReadSymptomDataFromFile (String filepath) { //Constructeur
 		this.filepath = filepath;
 	}
 	
 	@Override
-	public List<String> getSymptoms() {
-		ArrayList<String> result = new ArrayList<String>();
+	public List<String> getSymptoms() { //methode obligatoire qui est dans l'interface, elle renvoie une liste
+                                      
+		ArrayList<String> result = new ArrayList<String>(); // on crée une nouvelle liste
 		
-		if (filepath != null) {
-			try {
-				BufferedReader reader = new BufferedReader (new FileReader(filepath));
-				String line = reader.readLine();
+		if (filepath != null) {// si le fichier n'est pas vide on fait:
+			try {//tant qu'il n'y a pas d'erreur
+				BufferedReader reader = new BufferedReader (new FileReader(filepath)); 
+        //// Création d’un bufferedReader qui utilise le fileReader qui utilise le filepath(=Chemin d'accès)
+				String line = reader.readLine(); // lit la prochaine ligne
 				
-				while (line != null) {
-					result.add(line);
-					line = reader.readLine();
+				while (line != null) { // Tant que la ligne n'est pas vide: (Donc la boulce s'arréte si il ya un simple blanc?)
+					result.add(line); // ajoute la ligne à la list result
+					line = reader.readLine(); //lit la prochaine ligne
+
 				}
 				reader.close();
 			} catch (IOException e) {
